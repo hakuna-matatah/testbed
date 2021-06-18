6ab812db92fb99a38ef06cab7011473ed8492a57
Harish Kuna <hakuna@3c22fbe1d295.ant.amazon.com>
2021-06-17 20:43:49 -0700

    testbed comprising eks-cluster with flux integration for tests and addons

--- eks/bin/testbed.ts
+++ eks/bin/testbed.ts
@@ -4,7 +4,7 @@ import { TestbedStack, TestbedProps } from '../lib/testbed-stack';
 import { Repository } from '../lib/addons/fluxv2';
 
 const syncRepositories = [
-    { repoUrl: "https://github.com/awslabs/kubernetes-iteration-toolkit", repoBranch: "main", repoPath: "./testbed/k8s-config/" }];
+    { repoUrl: "https://github.com/hakuna-matatah/testbed", repoBranch: "testbed", repoPath: "./k8s-config/" }];
 const app = new cdk.App();
 new TestbedStack(app, 'TestbedStack', {
     env: {
