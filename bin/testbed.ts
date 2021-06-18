9eca6c1a9dc235d67436564ece40c6eabd3ef406
Harish Kuna <hakuna@3c22fbe1d295.ant.amazon.com>
2021-06-17 20:29:01 -0700

    testbed comprising eks-cluster with flux integration for tests and addons

--- bin/testbed.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { TestbedStack } from '../lib/testbed-stack';
-
-const app = new cdk.App();
-new TestbedStack(app, 'TestbedStack');
